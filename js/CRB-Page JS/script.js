3a269d36d44d77cbdd84b828af58171543b56646
Md. Sami Al Zaber <samizaber822@gmail.com>
2022-03-13 16:35:46 +0600

    first commit

---
+++ js/CRB-Page JS/script.js
@@ -0,0 +1,41 @@
+const playersDetail = () => {
+    window.scrollTo(0, 1150);
+    const detailsBody = document.getElementById('details');
+    detailsBody.textContent = "";
+    detailsBody.innerHTML = `
+                    <div class="col mx-auto">                    
+                        <div class="card player-detailCard">
+                                <div class="mx-auto mt-3">
+                                    <img src="images/CRB-Page/CRB-logo (Custom).png" alt="">
+                                </div>
+                                <div class="card-body ps-5">
+                                    <h2 class="card-title text-center">We Are working on it! <br> Please have patience.</h2>
+                                </div>
+                        </div>
+                    </div>
+`;
+    detailsBody.appendChild(div);
+}
+
+// show all players button click handler 
+const showMore = () => {
+    /* window.scrollTo(0, 1650);
+    const previousData = document.getElementById('previous-data');
+    previousData.style.display = 'none';
+    const newData = document.getElementById('new-data');
+    newData.style.display = 'block';
+    document.getElementById('show-more').style.display = 'none';
+    document.getElementById('hide').style.display = 'block'
+    document.getElementById('details').textContent = ""; */
+    window.location.href = ('playersList.html');
+}
+
+// hide players button click handler 
+const hidePlayers = () => {
+    /* document.getElementById('new-data').style.display = 'none'
+    document.getElementById('previous-data').style.display = 'block';
+    document.getElementById('show-more').style.display = 'block';
+    document.getElementById('hide').style.display = 'none'; */
+    window.location.href = ("crbPage.html");
+}
+
---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById('log-in').addEventListener('click', function () {
+    const userMail = document.getElementById('email');
+    const emailValue = userMail.value;
+
+    const password = document.getElementById('password');
+    const passwordValue = password.value;
+    if (emailValue == 'cumillaragingbulls@gmail.com' && passwordValue == 'crb12') {
+        window.location.href = '/CRB-Page/crb-page.html';
+    }
+    else {
+        const errorMessage = document.getElementById('error-handle');
+        errorMessage.style.display = 'block';
+    }
+}) 
