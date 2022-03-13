6841d143a1af47d35930505c89233106e289cca7
Md. Sami Al Zaber <samizaber822@gmail.com>
2022-03-13 23:11:46 +0600

    scrool error fixed removing aos

--- js/CRB-Page JS/script.js
+++ js/CRB-Page JS/script.js
@@ -38,4 +38,13 @@ const hidePlayers = () => {
     document.getElementById('hide').style.display = 'none'; */
     window.location.href = ("crbPage.html");
 }
+document.addEventListener('click', function (e) {
+    if (e.target.classList.contains("gallery-item")) {
+        const src = e.target.getAttribute("src");
+        console.log(src);
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'))
+        myModal.show();
+    }
+})
 
